03fa891f8cc6dca3ea7e2349ea642fd4dd2ee395
Mahmoud <Mahmoud@DESKTOP-V746SEF>
2025-09-29 15:11:27 +0200

    getting the main passive elements

--- CLLCDesign.m
+++ CLLCDesign.m
@@ -2,3 +2,55 @@
 clear;
 clc;
 close all;
+
+% The CLLC ratings definition
+
+P = 11e3;          % the rated power in Watts
+
+Vin_max = 800;     % the maximum input voltage in Volts
+Vin_nom = 750 ;    % the nominal input voltage in volts
+Vin_min = 700;     % the minimum input voltage in Volts
+
+Vout_max = 800;    % the maximum output voltage in Volts
+Vout_nom = 600 ;   % the nominal output voltage in volts
+Vout_min = 570;    % the minimum output voltage in Volts
+
+fr = 73e3;        % natural resonant frequency in Hz
+
+% Calculating the turn ratio for the transformer
+Nf = Vout_nom / Vin_nom;   % turns ratio in forward direction
+Nr = 1 / Nf;               % turns ratio in reverse direction
+
+% Calculating the minimum and maximum voltage gain
+
+Gf_min = (Nf * Vout_min) / Vin_max ; % minimum voltage gain in forward
+Gf_max = (Nf * Vout_max) / Vin_min ; % maximum voltage gain in forward
+Gr_min = (Nr * Vout_min) / Vin_max ; % minimum voltage gain in reverse
+Gr_max = (Nr * Vout_max) / Vin_min ; % maximum voltage gain in reverse
+
+% Choosing the suitable k and Q factors from GainCurves.m
+K = 4.45;
+Q = 0.3;
+
+% Calculating the effective resistance
+R0 = (8 * Nf^2 * Vout_nom^2) / (pi^2 * P);
+
+% Calculating the resonant capacitor C1
+C1 = 1 / (2*pi*Q*fr*R0);
+
+% Calculating the resonant inductor L1
+L1 = 1 / ((2*pi*fr)^2 * C1);
+
+% Calculating magnetizing inductance Lm
+Lm = K * L1;
+
+% Assuming a and b values
+a = 0.95;
+b = 1 / a;
+
+% Calculating the secondary-side resonant inductor and capacitor L2,C2
+L2 = (a * L1) / Nf^2;
+C2 = b * C1 * Nf^2;
+
+
+
